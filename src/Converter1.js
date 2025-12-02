c3d5bed80f5da5c927ff16145039aa4d273d2b0e
unknown <azfar12hussain@gmail.com>
2025-12-02 19:38:43 +0500

    complete crypto conversion code

--- src/Converter1.js
+++ src/Converter1.js
@@ -1,62 +1,89 @@
-import React, { useEffect } from 'react'
-import { Button, Card, Form, Input, Select,} from 'antd'
-import FormItem from 'antd/es/form/FormItem'
-import { Option } from 'antd/es/mentions'
+import React, { useEffect , useState} from 'react';
+import { Button, Card, Form, Input, Select,} from 'antd';
+import FormItem from 'antd/es/form/FormItem';
+import { Option } from 'antd/es/mentions';
+import Item from 'antd/es/list/Item';
+import { RiCoinLine } from 'react-icons/ri';
 
 function Converter1() {
     const apiUrl = 'https://api.coingecko.com/api/v3/exchange_rates';
-    const names = 
-    [
-        {
-            value: "jack",
-            leble: "Jack"
-        },
 
-         {
-            value: "luc",
-            leble: "Luc"
-        },
-
-         {
-            value: "yiminghe",
-            leble: "Yiming"
-        },
-
-    ];
+    const defaultFirstSelectValue = "Bitcoin" 
+    const defaultSecondSelectValue = "Ether" 
+   
+    const [cryptoList, setCryptoList] = useState([]);
+    const [inputValue, setInputValue] = useState("0");
+    const [firstSelect, setFirstSelect] = useState(defaultFirstSelectValue);
+    const [secondSelect, setSecondSelect] = useState(defaultSecondSelectValue);
+    const [result, setResult] = useState("0");
 
     
 
     useEffect(() => {
    
-        fetch(apiUrl);
+        fetchData();
 
     }, []);
+    
+    useEffect(() => {
+        
+        if(cryptoList.length == 0 ) return;
+        const firstSelectRate = cryptoList.find((item) => {
+            return item.value === firstSelect
+        }).rate
 
+        const secondSelectRate = cryptoList.find((item) => {
+            return item.value === secondSelect
+        }).rate
+        
+        const resultValue = (inputValue * secondSelectRate) / firstSelectRate
+        setResult(resultValue.toFixed(3))
+
+    }, [inputValue,firstSelect,secondSelect,cryptoList]);
     
     async function fetchData(params) {
         const response = await fetch(apiUrl);
         const jsonData = await response.json();
         
         const data = jsonData.rates
-        console.log(data);
+       
+        const tempArray = Object.entries(data).map(item => {
+                return {
+                value: item[1].name,
+                label: item[1].name,
+                rate: item[1].value
+            }
+        })
+
+        setCryptoList(tempArray);
     }
-
-
-    
 
   return (
     <div className='container'>
-        <Card className="crpto-card" title={<h1>cryto-converter</h1>}>
+        <Card className="crypto-card" title={<h1><RiCoinLine/>crypto-converter</h1>}>
             <Form>
                 <FormItem>
-                    <Input />
+                    <Input  onChange={(e) => setInputValue(e.target.value)}/>
                 </FormItem>
             </Form>
             <div className="select-box">
-                <Select options={names} defaultValue="lucy" style={{ width: 160 }} > </Select>
-                <Select options={names} defaultValue="lucy" style={{ width: 160 }} > </Select>
+                <Select
+                    options={cryptoList} 
+                    defaultValue={defaultFirstSelectValue} 
+                    style={{ width: 160 }}
+                    onChange={(value) => setFirstSelect(value)}
+                 > 
+                 </Select>
+                <Select 
+                    options={cryptoList} 
+                    defaultValue={defaultSecondSelectValue} 
+                    style={{ width: 160 }} 
+                    onChange={(value) => setSecondSelect(value)}
+                > 
+
+                </Select>
             </div>
-            <p>2 Bitcoin = 12 Ethereum</p>
+            <p>{inputValue}{firstSelect} = {result}{secondSelect}</p>
         </Card>
     </div>
     
